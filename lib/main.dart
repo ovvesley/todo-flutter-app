8e3820ec831f30cbdba34a6404dc02f038f8f276
Wesley Ferreira <ovvesley@gmail.com>
2020-10-17 13:11:29 -0300

    ListView: adding items

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() {
   runApp(App());
 }
@@ -20,17 +22,39 @@ class _AppState extends State<App> {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = generateItems();
+  }
+
+  List<Item> generateItems() {
+    var tempItems = new List<Item>();
+    tempItems = [];
+    tempItems.add(Item(done: true, title: "Item 0"));
+    tempItems.add(Item(done: false, title: "Item 1"));
+    tempItems.add(Item(done: false, title: "Item 2"));
+    tempItems.add(Item(done: true, title: "Item 3"));
+    tempItems.add(Item(done: true, title: "Item 4"));
+    return tempItems;
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("todo"),
         ),
-        body: Container(
-          child: Center(
-            child: Text("ola mundo"),
-          ),
-        ));
+        body: ListView.builder(
+            itemCount: widget.items.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Text(widget.items[index].title);
+            }));
   }
 }
